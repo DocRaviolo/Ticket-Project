68f65d9a38f99a344c5d205949ba2ae416ee55e2
Gilles <gilles.toni@sfr.fr>
2021-03-03 15:40:30 +0100

    2nd jet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-// On déclare un scanner pour lire les entrées de la console
+        // On déclare un scanner pour lire les entrées de la console
         Scanner sc = new Scanner(System.in);
 
-        // On affiche un message pour demander le numéro de table puis on récupère le numéro dans la console
+        //On affiche un message pour demander le numéro de table puis on récupère le numéro dans la console
         System.out.println("Numéro de table ? ");
         int numTable = sc.nextInt();
-
-        // On demande le produit
-        System.out.println("Produit ? : ");
-        String produit = sc.nextLine();
         sc.nextLine();
 
-        // On demande la quantité
-        System.out.println("Quantité ? : ");
-        int quantité = sc.nextInt();
-        sc.nextLine();
+        // on déclare toutes les variables utilisées
+        int compteur = 0;
+        int quantité = 0;
+        String produit = "";
+        float prix = 0;
+        boolean platSupp = true;
+        float sousTotal = 0;
+        ArrayList total = new ArrayList();
 
-        // On demande le prix unitaire
-        System.out.println("Prix unitaire ? ");
-        float prix = sc.nextFloat();
+        // on commence la boucle : tant que platSupp est vérifié, la boucle se répète
+        while (platSupp) {
+            // On demande le produit
+            System.out.println("Entrez le libellé produit : ");
+            produit = sc.nextLine();
 
-        // On demande si autre chose
-        System.out.println("Autre chose ? : ");
-        Boolean autrechose = sc.nextBoolean();
-        sc.nextLine();
+            // On demande la quantité
+            System.out.println("Quantité ? : ");
+            quantité = sc.nextInt();
+            sc.nextLine();
 
+            // On demande le prix unitaire
+            System.out.println("Prix unitaire ? ");
+            prix = sc.nextFloat();
+
+            // on fait le sous-total
+            sousTotal = quantité*prix;
+            System.out.println(sousTotal);
+            total.add(sousTotal);
+
+            // on demande s'il y a autre chose, c'est ici qu'on recommence la boucle si réponse true
+            System.out.println("Un plat ? : ");
+            platSupp = sc.nextBoolean();
+            sc.nextLine();
+                }
 
         // On vérifie le montant total
-        if (quantité*prix <= 0) {
+            System.out.println(total);
+        if (sousTotal <= 0) {
             System.out.println("FATAL ERROR !!!!!");
         } else {
             // On affiche le résultat
-            System.out.println("Le montant total pour la table " + numTable + " est de " + quantité*prix + ". Merci de votre visite.");
-        }
+            System.out.println("*************************************");
+            System.out.println(produit + " -----------------> " + quantité + "      " + quantité*prix + " EUR");
+            System.out.println("Le montant total pour la table " + " est de " + quantité * prix);
+            System.out.println("Merci de votre visite.");
+            System.out.println("*************************************");
 
+        }
         // On ferme le scanner
         sc.close();
     }
 }
+
