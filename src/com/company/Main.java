db8f6b86f439b47a84d7d2a4e603d7e73e863afd
Gilles <gilles.toni@sfr.fr>
2021-03-03 19:09:48 +0100

    Ticket Project rendu final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,44 +20,69 @@ public class Main {
         float prix = 0;
         boolean platSupp = true;
         float sousTotal = 0;
-        ArrayList total = new ArrayList();
+        ArrayList totalProduits = new ArrayList();
+        ArrayList totalQuantités = new ArrayList();
+        ArrayList<Float> total = new ArrayList<Float>();
+        float sum = 0;
 
         // on commence la boucle : tant que platSupp est vérifié, la boucle se répète
         while (platSupp) {
-            // On demande le produit
+            // On demande le produit consommé
             System.out.println("Entrez le libellé produit : ");
             produit = sc.nextLine();
+            totalProduits.add(produit);
 
             // On demande la quantité
             System.out.println("Quantité ? : ");
             quantité = sc.nextInt();
             sc.nextLine();
+            totalQuantités.add(quantité);
 
             // On demande le prix unitaire
             System.out.println("Prix unitaire ? ");
             prix = sc.nextFloat();
 
-            // on fait le sous-total
-            sousTotal = quantité*prix;
-            System.out.println(sousTotal);
+            // Calcul du sous-total
+            sousTotal = quantité * prix;
+            //System.out.println(sousTotal);
             total.add(sousTotal);
 
             // on demande s'il y a autre chose, c'est ici qu'on recommence la boucle si réponse true
             System.out.println("Un plat ? : ");
             platSupp = sc.nextBoolean();
             sc.nextLine();
-                }
+        }
 
+        //j'imprime les totaux de chaque liste pour vérification
+        //System.out.println(total);
+        //System.out.println(totalQuantités);
+        //System.out.println(totalProduits);
+
+        //calcul de la somme Totale
+        for (int i = 0; i < totalProduits.size(); i++) {
+            sum += total.get(i);
+            //   System.out.println(sum);
+        }
+
+        //Edition de la facture
         // On vérifie le montant total
-            System.out.println(total);
         if (sousTotal <= 0) {
             System.out.println("FATAL ERROR !!!!!");
         } else {
             // On affiche le résultat
-            System.out.println("*************************************");
-            System.out.println(produit + " -----------------> " + quantité + "      " + quantité*prix + " EUR");
-            System.out.println("Le montant total pour la table " + " est de " + quantité * prix);
+            System.out.println("*************** FACTURE *******************");
+            System.out.println(" ");
+            System.out.println("Table n° " + numTable);
+            System.out.println(" ");
+            //boucle pour printer le récap des produits, quantités et prix total
+            for (int i = 0; i < totalProduits.size(); i++) {
+                System.out.println(totalProduits.get(i) + " --------------> " + totalQuantités.get(i) + "    " + total.get(i));
+            }
+            System.out.println(" ");
+            System.out.println("TOTAL ----------------> " + sum + " EUR");
+            System.out.println(" ");
             System.out.println("Merci de votre visite.");
+            System.out.println(" ");
             System.out.println("*************************************");
 
         }
